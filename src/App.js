4a9918eaf1167fbe0481838a5838462f5152493d
restrada <rene1tres@gmail.com>
2020-10-30 20:05:49 -0600

    users list component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Users from './components/users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const dotenv = require('dotenv');
+dotenv.config();
+const usersApi = process.env.API_URL;
+
+class App extends Component {
+    render() {
+        return (
+            <Users users={this.state.users}/>
+        )
+    }
+
+    state = {
+        users: []
+    };
+
+    componentDidMount() {
+        const localOnly = 'http://localhost:3000/api/users/';
+
+        fetch((usersApi === undefined) ? localOnly : usersApi + 'users/')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({users: data})
+            })
+            .catch(console.log)
+    }
 }
 
 export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,9 @@
+function Welcome() {
+    return (
+        <div className="Welcome">
+            <h1>Hello, world jes test!</h1>,
+        </div>
+    );
+}
+
+export default Welcome;
